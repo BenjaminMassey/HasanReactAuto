ad8ffef4b51d285ce19715e6350c9c2db9fdeb22
Benjamin Massey <benjamin.w.massey@gmail.com>
2024-03-14 13:07:43 -0700

    Have it setup so that a GPT service can be used as a backup name generator, in case one is not gathered. This is done with a local GPT instance in mind, like GPT4All, and while the URL is in a changeable constant, authentication is not bothered with.

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,21 @@
-use chatgpt::prelude::*;
 use enigo::*;
 use glob::glob;
 use raster::filter;
 use screenshots::Screen;
 use std::{fmt, io, thread, time};
-use tokio;
 
 const DEBUG_MESSAGES: bool = true;
 const CAPTURE_PATH: &str = "./capture.png";
-const VIDEOS_PATH: &str = "C:/Users/benja/Videos/";
+const IN_VIDEOS_PATH: &str = "C:\\Users\\benja\\Videos\\";
+const OUT_VIDEOS_PATH: &str = "C:\\Users\\benja\\Videos\\HRA\\";
 const VIDEO_EXT: &str = ".mp4";
 const START_REC_HELD: &[Key] = &[Key::Control, Key::Alt];
 const START_REC_CLICK: Key = Key::F6;
 const STOP_REC_HELD: &[Key] = &[Key::Control, Key::Alt];
 const STOP_REC_CLICK: Key = Key::F7;
+const GPT_URL: &str = "127.0.0.1:4891";
+const GPT_MODEL: &str = "Nous Hermes 2 Mistral DPO";
+const GPT_ATTEMPTS: usize = 5;
 
 #[derive(Clone, Copy)]
 struct CaptureArea {
@@ -216,8 +218,17 @@ fn try_get_title(yt_url: &str) -> Option<String> {
     Some(title.unwrap().to_string())
 }
 
+fn path_clean(path: &str) -> String {
+    path
+        .to_owned()
+        .replace("\"", "")
+        .replace("\\", "")
+        .replace("/", "")
+        .replace(".", "")
+}
+
 fn update_title(title: Option<String>, captions: Vec<String>) {
-    let vid_paths = glob(&(VIDEOS_PATH.to_owned() + "*" + &VIDEO_EXT))
+    let vid_paths = glob(&(IN_VIDEOS_PATH.to_owned() + "*" + &VIDEO_EXT))
         .unwrap()
         .filter_map(std::result::Result::ok);
     let mut vids = vid_paths
@@ -227,51 +238,104 @@ fn update_title(title: Option<String>, captions: Vec<String>) {
     let possible_vid = vids.last();
     let the_title: Option<String> = {
         if let Some(retrieved) = title {
-            Some(retrieved)
+            Some("Hasan reacts to ".to_owned() + &retrieved)
         } else {
-            let openai_key = std::env::args().nth(1).unwrap();
-            let gpt_client = ChatGPT::new_with_config(
-                openai_key,
-                ModelConfigurationBuilder::default()
-                    .engine(ChatGPTEngine::Gpt4)
-                    .build()
-                    .unwrap(),
-            ).unwrap();
-            let runtime = tokio::runtime::Runtime::new().expect("Unable to create a runtime");
-            let possible_title: Result<String> = runtime.block_on(gpt_title(gpt_client, captions));
-            if let Ok(generated) = possible_title {
-                Some(generated)
-            } else {
-                None
+            let mut result = None;
+            for _ in 0..GPT_ATTEMPTS {
+                let answer = gpt_title(&captions);
+                if let Some(text) = answer {
+                    if text.len() > 0 && text.len() < 50 {
+                        result = Some(text.clone());
+                        break;
+                    }
+                }
             }
+            result
         }
     };
     if let Some(final_title) = the_title {
-        if let Some(vid) = possible_vid {
-            std::fs::rename(
-                vid, VIDEOS_PATH.to_owned() + &final_title + &VIDEO_EXT
-            ).unwrap();
+        if let Some(source) = possible_vid {
+            let destination = OUT_VIDEOS_PATH.to_owned()
+                + &path_clean(&final_title) + &VIDEO_EXT;
+            let rename = std::fs::rename(source, &destination);
+            if rename.is_err() {
+                println!("Rename failed. File: {source}");
+            } else {
+                println!("Rename success. File: {destination}");
+            }
         }
     }
 }
 
-async fn gpt_title(client: ChatGPT, captions: Vec<String>) -> Result<String> {
+fn local_gpt_body(message: &str) -> String {
+    format!(
+        r#"
+        {{
+            "model": "{GPT_MODEL}",
+            "max_tokens": 100,
+            "messages": [
+                {{
+                    "role": "system",
+                    "content": "You are a helpful assistant."
+                }},
+                {{
+                    "role": "user",
+                    "content": "{message}"
+                }}
+            ]
+        }}
+        "#
+    )
+}
 
-    let mut message = "I have a lot of text gathered from a video. The video
-                       is of a Twitch streamer reacting to a video. The text
-                       will include captions of both the streamer reacting
-                       and the video being watched itself. Based on the
-                       following text, what short and concise description
-                       would you give the video that is being reacted to?
-                       Here are the captions: ".to_owned();
+fn local_gpt_chat(message: &str) -> Option<String> {
+    let url = "http://".to_owned() + &GPT_URL + "/v1/chat/completions";
+    let client = reqwest::blocking::Client::new();
+    let body = local_gpt_body(message);
+    let result = client.post(url).body(body).send();
+    if result.is_err() {
+        return None;
+    }
+    let json = serde_json::from_str(&result.unwrap().text().unwrap());
+    if json.is_err() {
+        return None;
+    }
+    let value: serde_json::Value = json.unwrap();
+    let choices = value.get("choices");
+    if choices.is_none() {
+        return None;
+    }
+    let message = choices.unwrap()[0].get("message");
+    if message.is_none() {
+        return None;
+    }
+    let content = message.unwrap().get("content");
+    if content.is_none() {
+        return None;
+    }
+    Some(content.unwrap().to_string())
+}
+
+fn gpt_title(captions: &Vec<String>) -> Option<String> {
+
+    let mut all_captions = String::new();
 
     let length = captions.len();
     let range = std::cmp::min(50, length);
     for i in 0..range {
-        message = message + &captions[(length / range) * i] + " ";
+        all_captions = all_captions + &captions[(length / range) * i] + " ";
     }
 
-    let response: chatgpt::types::CompletionResponse = client.send_message(message).await?;
+    let message = format!(
+        "I have a lot of text gathered from a video. The video
+        is of a Twitch streamer reacting to a video. The text
+        will include captions of both the streamer reacting
+        and the video being watched itself. Here are the captions:
+        {all_captions}. Based on those captions, what short and 
+        what would you guess is the title of video that is being
+        reacted to? Reply with only the text 'Hasan reacts to' followed
+        by your video title guess."
+    );
 
-    Ok(response.message().content.to_owned())
+    local_gpt_chat(&message)
 }
